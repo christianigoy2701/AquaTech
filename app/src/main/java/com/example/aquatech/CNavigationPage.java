a466c6e333621fd66efb4d7ac4a1fcf4d9be135b
Igoy <christianigoy.official2701@gmail.com>
2022-12-11 20:39:12 +0800

    AquaTech

--- app/src/main/java/com/example/aquatech/CNavigationPage.java
+++ app/src/main/java/com/example/aquatech/CNavigationPage.java
@@ -7,41 +7,37 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
-import com.example.aquatech.databinding.ActivityMainBinding;
-import com.example.aquatech.databinding.ActivityNavigationPageBinding;
+import com.example.aquatech.databinding.ActivityCnavigationPageBinding;
 
-public class NavigationPage extends AppCompatActivity {
+public class CNavigationPage extends AppCompatActivity {
 
-    ActivityNavigationPageBinding binding;
+    ActivityCnavigationPageBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityNavigationPageBinding.inflate(getLayoutInflater());
+        binding = ActivityCnavigationPageBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        replaceFragment(new AHomePage());
+        replaceFragment(new CHomePage());
 
-        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+        binding.bottomCNavigationView.setOnItemSelectedListener(item -> {
 
             switch (item.getItemId()){
                 case R.id.homeButton:
-                    replaceFragment(new AHomePage());
-                    break;
-                case R.id.searchButton:
-                    replaceFragment(new ASearchPage());
+                    replaceFragment(new CHomePage());
                     break;
                 case R.id.chatButton:
-                    replaceFragment(new MChangePassword());
+                    replaceFragment(new CProfileEditPage());
                     break;
                 case R.id.profileButton:
-                    replaceFragment(new AProfilePage());
+                    replaceFragment(new CChangePassword());
                     break;
             }
             return true;
         });
     }
 
-    private void replaceFragment(Fragment fragment){
+    private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frameLayout,fragment);
